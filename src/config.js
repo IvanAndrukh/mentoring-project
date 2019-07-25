fdf96b82ce0eaac621f6878de38478b78a6530ab
Ivan Andrukh <ivanandrukh@gmail.com>
2019-07-25 09:13:44 +0300

    Add Ava and Sinon testing tools to the project

--- src/config.js
+++ src/config.js
@@ -1,9 +1,16 @@
 const secureEnv = require('secure-env');
 
+const env = process.env.NODE_ENV || 'development';
 const envConfig = secureEnv({ secret: process.env.PROJECT_ENV_KEY });
 const { MYSQL_PASS } = envConfig;
 
-module.exports = {
-  mongoDbUrl: 'mongodb://127.0.0.1:27017,127.0.0.1:27018,127.0.0.1:27019/nosql-db',
-  mysqlPass: MYSQL_PASS,
+const config = {
+  development: {
+    mongoDbUrl: 'mongodb://127.0.0.1:27017,127.0.0.1:27018,127.0.0.1:27019/nosql-db',
+    mysqlPass: MYSQL_PASS,
+  },
+  test: { },
 };
+
+
+module.exports = config[env];
